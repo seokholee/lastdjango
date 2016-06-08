04597c674b5799b800d81f198163081eb1a8c3c9
seokholee <ssda0739@gmail.com>
2016-06-08 11:33:21 +0900

    blog.post_list 뷰에서 Post 목록 노출

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'blog/index.html')
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'post_list' : post_list,
+    })
