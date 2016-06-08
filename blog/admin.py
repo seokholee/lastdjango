cc47acf43ae4312b22bcaec9624eb0f28d989359
seokholee <ssda0739@gmail.com>
2016-06-08 11:30:33 +0900

    blog.Post 모델을 admin에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content_size', 'created_at']
+
+    def content_size(self, post):
+        return '%s자' % intcomma(len(post.content))
+
+admin.site.register(Post, PostAdmin)
