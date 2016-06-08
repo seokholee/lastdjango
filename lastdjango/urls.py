4973d760a26076a1fdfb16679d06ac3d3e61f9e9
seokholee <ssda0739@gmail.com>
2016-06-08 11:26:23 +0900

    blog 앱 추가 및 blog.views.index 뷰 구현

--- lastdjango/urls.py
+++ lastdjango/urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^$', lambda request: redirect('/blog/')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
