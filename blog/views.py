c0929e49a701730c9acb70d97f10edfcf33d7181
seokholee <ssda0739@gmail.com>
2016-06-08 12:26:58 +0900

    blog.Comment 댓글 등록 시에 post/user 필드를 코드를 통해 등록

--- blog/views.py
+++ blog/views.py
@@ -14,11 +14,15 @@ def post_detail(request, pk):
         'post' : post,
     })
 
+@login_required
 def comment_new(request, post_pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save()
+            comment = form.save(commit=False)
+            comment.post = get_object_or_404(Post pk=post_pk)
+            comment.user = request.user
+            comment.save()
             return redirect('blog:post_detail', post_pk)
     else:
         form = CommentForm()
