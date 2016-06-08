aa56c83c2690fb631a360b5a8e31f4b48fb0734a
seokholee <ssda0739@gmail.com>
2016-06-08 12:29:53 +0900

    댓글 수정 구현

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 def index(request):
@@ -26,6 +26,25 @@ def comment_new(request, post_pk):
             return redirect('blog:post_detail', post_pk)
     else:
         form = CommentForm()
+    return render(request, 'blog/comment_form.html', {
+        'form': form,
+    })
+
+
+@login_required
+def comment_edit(request, post_pk, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = get_object_or_404(Post, pk=post_pk)
+            comment.user = request.user
+            comment.save()
+            return redirect('blog:post_detail', post_pk)
+    else:
+        form = CommentForm(instance=comment)
     return render(request, 'blog/comment_form.html', {
         'form': form,
     })
